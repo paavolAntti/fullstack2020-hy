bb21f620342d7cf4fb8d8f179a080fb644ffd858
Antti Paavola <antti.paavis@gmail.com>
2020-02-22 09:23:07 +0200

    deleted api key from code, used process.env instead

--- osa2/maiden_tiedot/src/components/SoloCountry.js
+++ osa2/maiden_tiedot/src/components/SoloCountry.js
@@ -3,11 +3,12 @@ import axios from 'axios'
 
 const SoloCountry = ({country}) => {
         const [ weather, setWeather] = useState({})
-
+		const api_key = process.env.REACT_APP_API_KEY
+		console.log('api_key: ', api_key)
         const hook = () => {
-            console.log('using weather effect hook')
+			console.log('using weather effect hook')
             axios
-            .get('http://api.weatherstack.com/current', {params: {access_key: '67d16cc10d71527d7e9bbeacdf1d245b', query: country.capital}})
+            .get('http://api.weatherstack.com/current', {params: {access_key: api_key , query: country.capital}})
             .then(response => {
               console.log('countries fetched')
               //console.log(response.data.current)
@@ -41,7 +42,7 @@ const SoloCountry = ({country}) => {
                 <div>
                     <h2>Weather in {location.name}</h2>
                     <p>Temperature: {current.temperature} Celcius</p>
-                    <img src={current.weather_icons} width='20%' height='20%'/>
+                    <img src={current.weather_icons} width='20%' height='20%' alt='weather_icon'/>
                     <p>Wind: {current.wind_speed} direction: {current.wind_dir}</p>
                 </div> 
             </div>
