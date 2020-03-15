9eab44fe96171e13d57cad366c3f936a342473c5
Antti Paavola <banpa001@edu.xamk.fi>
2020-03-15 15:35:37 +0200

    uibuild

---
+++ osa2/puhelinluettelo/build/precache-manifest.91fab48010419f36628949cd2224caf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56699c9e45f30fc078bf5683b7ebcb42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3627c8b9d3cb3d3a54e0",
+    "url": "/static/css/main.67c31f5e.chunk.css"
+  },
+  {
+    "revision": "8514db5cee128984cd03",
+    "url": "/static/js/2.16d1eaee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.16d1eaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3627c8b9d3cb3d3a54e0",
+    "url": "/static/js/main.8f457a94.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
